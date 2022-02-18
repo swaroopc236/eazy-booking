ee2f168fdeac420bcd316040052218268c510b8a
swaroopc236 <swaroopc236@gmail.com>
2022-02-18 22:48:57 +0530

    room edit functionality

--- src/app/features/room/room-edit/room-edit.component.ts
+++ src/app/features/room/room-edit/room-edit.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RoomService } from '../services/room.service';
 
 @Component({
   selector: 'app-room-edit',
   templateUrl: './room-edit.component.html',
-  styleUrls: ['./room-edit.component.css']
+  styleUrls: ['./room-edit.component.css'],
 })
 export class RoomEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  roomForm: FormGroup;
+  roomData: any;
+  roomDetails: any = {
+    roomId: '',
+    roomName: '',
+    roomDetails: null,
+  };
+  constructor(
+    private roomService: RoomService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.roomData = this.router.getCurrentNavigation()?.extras.state;
+    this.roomDetails.roomId = this.roomData.roomData.roomId;
+    this.roomDetails.roomName = this.roomData.roomData.roomName;
+    this.roomDetails.roomDetails = this.roomData.roomData.roomDetails;
+    this.roomForm = this.fb.group({
+      roomName: [this.roomDetails.roomName, [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  editRoom() {
+    this.roomDetails.roomName = this.roomForm.value['roomName'];
+    this.roomService.editRoom(this.roomDetails).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.router.navigateByUrl('/admin');
+      },
+      (err) => {
+        console.log('Error in editing room', err);
+      }
+    );
+  }
 }
