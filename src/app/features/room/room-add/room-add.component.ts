80ed8184390a03016ef152abeeb24f5e04c9f056
swaroopc236 <swaroopc236@gmail.com>
2022-02-18 22:49:15 +0530

    room add functionality

--- src/app/features/room/room-add/room-add.component.ts
+++ src/app/features/room/room-add/room-add.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RoomService } from '../services/room.service';
 
 @Component({
   selector: 'app-room-add',
   templateUrl: './room-add.component.html',
-  styleUrls: ['./room-add.component.css']
+  styleUrls: ['./room-add.component.css'],
 })
 export class RoomAddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  roomForm: FormGroup;
+  roomDetails: any = {
+    roomName: '',
+    roomDetails: null,
+  };
+  constructor(
+    private roomService: RoomService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.roomForm = this.fb.group({
+      roomName: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  addRoom() {
+    this.roomDetails.roomName = this.roomForm.value['roomName'];
+    this.roomService.addRoom(this.roomDetails).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.router.navigateByUrl('/admin');
+      },
+      (err) => {
+        console.log('Error in adding room', err);
+      }
+    );
+  }
 }
