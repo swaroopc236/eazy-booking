048711c6ca87067fd357302f65ff90b345ce7cd6
Mandar Mahesh Prabhu <mandar.prabhu9@gmail.com>
2022-03-02 17:09:54 +0530

    Build Changes

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
-import { NgxSpinnerService } from 'ngx-spinner';
+// import { NgxSpinnerService } from 'ngx-spinner';
 import { EventService } from '../booking/services/event.service';
 import { AuthService } from '../services/auth.service';
 
@@ -37,7 +37,7 @@ export class EventsComponent implements OnInit {
     private eventService: EventService,
     private authService: AuthService,
     private cookieService: CookieService,
-    private spinnerService: NgxSpinnerService,
+    // private spinnerService: NgxSpinnerService,
     private router: Router,
     private route: ActivatedRoute
   ) {
@@ -79,32 +79,35 @@ export class EventsComponent implements OnInit {
 
     console.log(this.event);
 
-    this.eventService.isOverlapping(this.event, this.selectedDate).then(isOverlap => {
-      if(isOverlap) {
-        this.errormsg = 'The event you are trying to create conflicts with another event. Try changing the time or room.'
-      } else {
-        this.spinnerService.show();
-        this.eventService.addEvent(this.event, this.selectedDate).subscribe(
-          (data: any) => {
-            this.spinnerService.hide();
-            this.router.navigate(['/schedule'], {
-              queryParams: {
-                selectedRoomId: this.roomId,
-                selectedDate: this.selectedDate,
-              },
-              replaceUrl: true,
-            });
-          },
-          (err) => {
-            console.log('Error in adding event', err);
-            this.spinnerService.hide();
-          }
-        );
+    this.eventService.isOverlapping(this.event, this.selectedDate).then(
+      (isOverlap) => {
+        if (isOverlap) {
+          this.errormsg =
+            'The event you are trying to create conflicts with another event. Try changing the time or room.';
+        } else {
+          // this.spinnerService.show();
+          this.eventService.addEvent(this.event, this.selectedDate).subscribe(
+            (data: any) => {
+              // this.spinnerService.hide();
+              this.router.navigate(['/schedule'], {
+                queryParams: {
+                  selectedRoomId: this.roomId,
+                  selectedDate: this.selectedDate,
+                },
+                replaceUrl: true,
+              });
+            },
+            (err) => {
+              console.log('Error in adding event', err);
+              // this.spinnerService.hide();
+            }
+          );
+        }
+      },
+      (err) => {
+        console.log(err);
       }
-
-    }, (err) => {
-      console.log(err);
-    });
+    );
   }
 
   onCancel() {
--- src/app/features/room/room-edit/room-edit.component.ts
+++ src/app/features/room/room-edit/room-edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
+// import { NgxSpinnerService } from 'ngx-spinner';
 import { RoomService } from '../services/room.service';
 
 @Component({
@@ -21,7 +21,7 @@ export class RoomEditComponent implements OnInit {
   constructor(
     private roomService: RoomService,
     private fb: FormBuilder,
-    private spinnerService: NgxSpinnerService,
+    // private spinnerService: NgxSpinnerService,
     private router: Router
   ) {
     this.roomData = this.router.getCurrentNavigation()?.extras.state;
@@ -37,17 +37,17 @@ export class RoomEditComponent implements OnInit {
 
   editRoom() {
     this.roomDetails.roomName = this.roomForm.value['roomName'];
-    this.spinnerService.show();
+    // this.spinnerService.show();
     this.roomService.editRoom(this.roomDetails).subscribe(
       (data: any) => {
         console.log(data);
-        this.spinnerService.hide();
+        // this.spinnerService.hide();
         this.router.navigateByUrl('/admin', { replaceUrl: true });
       },
       (err) => {
         this.errormsg = err.error.msg;
         console.log('Error in editing room', err);
-        this.spinnerService.hide();
+        // this.spinnerService.hide();
       }
     );
   }
